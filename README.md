ad81efa53266497d32ff9d815fd26b83d3551759
Mattias M <TheMatteM1@gmail.com>
2022-02-14 10:33:47 +0100

    changes to program

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Mitt projekt API
 
 - Den gissar din ålder beroende på ditt namn.
+
+installera requests med hjälp av pip i cmd.
+
+## License
+
+- [MIT] (https://choosealicense.com/licenses/mit/)
--- api_project.py
+++ api_project.py
@@ -1,11 +1,21 @@
 import requests
 
 
-req = requests.get('https://api.agify.io?name=Fredrik')
 class Name:
     def __init__(self, name):
             self.name = name
+    
+    def name_input(self, name):
+        req = requests.get(f'https://api.agify.io?name={name}')
+        show = req.json()
+        print('Name:' , show['name'], 'Age: ', show['age'])
 
-show = req.json()
 
-print(show)
+user = Name('namn')
+
+user_input = ''
+while user_input != 'exit':
+    user_input = str(input('Skriv ett namn: '))
+    if user_input == 'exit':
+        break
+    user.name_input(user_input)
