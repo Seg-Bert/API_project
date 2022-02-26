bf278d4e70235167e1fa16e084bb6a9013033eb9
Mattias M <TheMatteM1@gmail.com>
2022-02-26 23:28:49 +0100

    updated program and README

--- README.md
+++ README.md
@@ -2,8 +2,27 @@
 
 - Den gissar din ålder beroende på ditt namn.
 
-installera requests med hjälp av pip i cmd.
+## Programmeringspråk/installation
 
-## License
+hela programmet är skrivet i python.
+
+- Programmet använder requests (python).
+
+installera requests med hjälp av pip i cmd. Skriv "pip install requests" i cmd.
+
+## Om programmet
+
+Användaren frågas om input som sedan kollas av en if-sats ifall inputen är för lång (API:n kan max ta 30 tecken).
+Ifall inputen är giltig skickas den in i klassen och då skickas begäran till API:n och sedan får användaren ut namnet som skrevs in och medelåldern på valda namnet.
+
+## Att göra
+
+API:n förstår inte siffror, vid input med siffror blir output "none" för ålder. Detta bör åtgärdas.
+
+## Bidra
+
+Detta är en skolprojekt så accpeterar inga pull requests.
+
+## Licens
 
 - [MIT] (https://choosealicense.com/licenses/mit/)
--- api_project.py
+++ api_project.py
@@ -2,20 +2,37 @@ import requests
 
 
 class Name:
+
+
     def __init__(self, name):
             self.name = name
     
     def name_input(self, name):
         req = requests.get(f'https://api.agify.io?name={name}')
+
         show = req.json()
         print('Name:' , show['name'], 'Age: ', show['age'])
 
+        """req anropar API:n som sedan hämtar tillbaks informationen som läggs till i show som sedan skrivs ut"""
 
 user = Name('namn')
 
 user_input = ''
 while user_input != 'exit':
-    user_input = str(input('Skriv ett namn: '))
+    check = True
+    while check == True:
+        user_input = str(input('Skriv ett namn: '))
+
+        if len(user_input) >= 30:
+            print('\nnamnet du skrev in är för långt, försök igen.\n')
+        
+        else:
+            check = False
+    
     if user_input == 'exit':
         break
+
     user.name_input(user_input)
+
+    """användaren får skriva in ett namn till user_input som sedan kollas av en if-sats ifall namnet är för lång med hjälp av en bool.
+       Ifall namnet är rätt längd skickas den in i metoden name_input"""
